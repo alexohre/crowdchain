e207bbc411ed08ac8bae63621dc90d3eca5c5d57
ussyalfaks <usmanalfaki@gmail.com>
2025-03-23 14:37:35 +0100

    feat: add Featured Projects and How It Works components to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 import React from 'react'
 import HeroSection from './components/Hero'
+import HowItWorks from './components/HowItWorks'
+import FeaturedProjects from './components/FeaturedCampaign'
 
 function Home() {
   return (
     <div>
         <HeroSection/>
+        <HowItWorks/>
+        <FeaturedProjects/>
+
     </div>
   )
 }
