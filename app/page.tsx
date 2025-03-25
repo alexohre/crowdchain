dd415ed9e548a9453c09a3b819f6551439cc6090
Goodnessukaigwe <goodnessukaigwe2020@gmail.com>
2025-03-25 17:53:21 +0100

    feat: add Explore Categories component and new SVG icons

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HeroSection from './components/Hero'
 import HowItWorks from './components/HowItWorks'
 import FeaturedProjects from './components/FeaturedCampaign'
+import ExploreCategories from './components/ExploreCatergories'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
         <HeroSection/>
         <HowItWorks/>
         <FeaturedProjects/>
+        <ExploreCategories/>
 
     </div>
   )
