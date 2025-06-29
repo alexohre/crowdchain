691366f167f439b2e95efbca0b2281070151f9bf
Collins Ikechukwu <collinschristroa@gmail.com>
2025-06-29 15:44:54 +0100

    refactor: update grid layout in FeaturedCampaign component
    - Removed commented-out grid layout code for clarity.
    - Adjusted grid classes to enhance responsiveness and maintainability.

--- app/components/FeaturedCampaign.tsx
+++ app/components/FeaturedCampaign.tsx
@@ -10,7 +10,6 @@ export default function FeaturedProjects() {
       <h2 className="text-3xl font-bold mb-8 text-[#1A5D1A] md:text-[#000000]">
         Featured Projects
       </h2>
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-6">
         {featuredCampaigns.map((campaign) => (
           <Link href={`/campaigns/${campaign.slug}`} key={campaign.id}>
