2736fd0a109b576ceac43f29f19235a01fe55695
Collins Ikechukwu <collinschristroa@gmail.com>
2025-06-29 15:43:48 +0100

    feat: update FeaturedCampaign component for dynamic routing
    - Wrapped CampaignCard components in a Link for dynamic routing to campaign details.
    - Adjusted layout to improve responsiveness with updated grid classes.
    - Enhanced section styling for better alignment and presentation.

--- app/components/FeaturedCampaign.tsx
+++ app/components/FeaturedCampaign.tsx
@@ -1,28 +1,34 @@
 import { campaigns } from "../data/campaign";
 import CampaignCard from "./CampaignCard";
 import { calculateDaysLeft } from "../utils/slugify";
+import Link from "next/link";
 
 export default function FeaturedProjects() {
   const featuredCampaigns = campaigns.slice(0, 3);
   return (
-    <section className="w-full px-6 md:px-9 lg:px-16 max-w-7xl mx-auto mt-10">
+    <section className="max-w-7xl flex flex-col justify-center items-center mx-auto">
       <h2 className="text-3xl font-bold mb-8 text-[#1A5D1A] md:text-[#000000]">
         Featured Projects
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-6">
         {featuredCampaigns.map((campaign) => (
-          <CampaignCard
-            key={campaign.id}
-            id={campaign.id}
-            title={campaign.title}
-            description={campaign.description || "No description available"}
-            image={campaign.image}
-            raisedAmount={campaign.raised.toString()}
-            daysLeft={campaign.endDate ? calculateDaysLeft(campaign.endDate) : 0}
-            progress={Math.round((campaign.raised / campaign.goal) * 100)}
-            status={campaign.status}
-            slug={campaign.slug}
-          />
+          <Link href={`/campaigns/${campaign.slug}`} key={campaign.id}>
+            <CampaignCard
+              key={campaign.id}
+              id={campaign.id}
+              title={campaign.title}
+              description={campaign.description || "No description available"}
+              image={campaign.image}
+              raisedAmount={campaign.raised.toString()}
+              daysLeft={
+                campaign.endDate ? calculateDaysLeft(campaign.endDate) : 0
+              }
+              progress={Math.round((campaign.raised / campaign.goal) * 100)}
+              status={campaign.status}
+              slug={campaign.slug}
+            />
+          </Link>
         ))}
       </div>
     </section>
