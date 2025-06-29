78e7c9c4545504b284877623be0c5e9726e02937
martinvibes <martinmachiebe21@gmail>
2025-06-29 18:15:18 +0100

    feat: implement connect connect starknet provider and wrap it up in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { StarknetProvider } from "./components/starknet-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,24 +25,26 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Navbar />
-        <main>
-          {children}
-          <Toaster
-            position="top-center"
-            toastOptions={{
-              style: {
-                background: "#1A5D1A",
-                color: "#fff",
-                padding: "10px 20px",
-                borderRadius: "5px",
-                fontSize: "16px",
-              },
-              duration: 3000,
-            }}
-          />
-        </main>
-        <Footer />
+        <StarknetProvider>
+          <Navbar />
+          <main>
+            {children}
+            <Toaster
+              position="top-center"
+              toastOptions={{
+                style: {
+                  background: "#1A5D1A",
+                  color: "#fff",
+                  padding: "10px 20px",
+                  borderRadius: "5px",
+                  fontSize: "16px",
+                },
+                duration: 3000,
+              }}
+            />
+          </main>
+          <Footer />
+        </StarknetProvider>
       </body>
     </html>
   );
